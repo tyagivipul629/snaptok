10aa369903dff7bcbea15ef84710ebe8eaf2fd33
tyagivipul629 <vipultyagi629@gmail.com>
2021-05-23 22:54:03 +0530

    make it bettr

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const SocialPost=require('./CollectionsSchema.js')
 const multer=require('multer')
 const cors=require('cors')
 const {Storage} = require('@google-cloud/storage')
+const fs=require('fs')
 
 const storage = new Storage({
     projectId: "gratis-3ce5a",
@@ -22,9 +23,11 @@ app.use(cors());
 var storage1 = multer.diskStorage({
 	destination: function (req, file, cb) {
 		// Uploads is the Upload_folder_name
+		if(!file) return;
 		cb(null, "files")
 	},
 	filename: function (req, file, cb) {
+	if(!file) return;
 	var arr=file.mimetype.split("/");
 	var extension=arr[1]
 	cb(null, file.fieldname + "-" + Date.now()+"."+extension)
@@ -38,14 +41,22 @@ const upload = multer({
 
 
 app.post('/post',upload.single('file'),(req,res)=>{
-	console.log(req.body);
-	console.log(req.file);
-bucket.upload(req.file.path,{destination: 'images/'+req.file.filename},function(err,file,response){
-        //console.log(file);
+	
+bucket.upload(req.file.path,{destination: 'files/'+req.file.filename},function(err,file,response){
         if(err) throw err;
-        else res.send('https://storage.googleapis.com//'+bucket.name);
+        else{
+			fs.unlink(req.file.path,(err)=>{if(err) throw err;})
+			req.body.file=file.metadata.mediaLink;
+			var newPost=new SocialPost(req.body);
+			console.log(newPost);
+			newPost.save().then((item)=>{
+				res.json({"Status":"Success"});
+			}).catch(err=>{
+				console.log(err);
+				res.status(400).json({"Status":"Failure"});
+			})
+		}
     })
-    //res.send(JSON.stringify({'status':'success'}))
 })
 
 
