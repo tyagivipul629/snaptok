c8cfd8d2be64cafe4f12882b1d81ca3c80aadfe5
tyagivipul629 <vipultyagi629@gmail.com>
2021-06-23 09:38:42 +0530

    FetchPost category

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/deleteComment',(req,res)=>{
 })
 
 app.get('/fetchPosts',(req,res)=>{
-	SocialPost.find({},{comments: 0,description: 0},{sort: {dateTime: -1}}, function(err,result){
+	SocialPost.find({category: req.body.category},{comments: 0,description: 0},{sort: {dateTime: -1}}, function(err,result){
 		if(err) res.send(err);
 		else res.json(result);
        })
